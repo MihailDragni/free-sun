758524a35f992f68d7f01fec94d0893101e15c5b
MihailDragni <74327586+MihailDragni@users.noreply.github.com>
2022-06-22 18:29:14 +0300

    added hero section

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,52 @@
-document.addEventListener('DOMContentLoaded', () => {
-      const burger = document.querySelector('.burger');
-      burger.addEventListener('click', () => {
-        burger.classList.toggle('burger--active');
-      });
-    });
+document.addEventListener("DOMContentLoaded", () => {
+  const burger = document.querySelector(".burger");
+  const burgerMenu = document.querySelector(".burger-menu");
+  const bodyLock = document.querySelector("body");
+  const burgerMenuWrapper = document.querySelector(".burger-menu__wrapper");
+  const menuBtn = document.querySelector(".menu-btn");
+
+  burger.addEventListener("click", () => {
+    burger.classList.toggle("burger--active");
+    burgerMenu.classList.toggle("burger-menu--active");
+    if (burgerMenu.classList.contains("burger-menu--active")) {
+      burger.classList.add("burger--active");
+      bodyLock.classList.add("lock");
+    } else {
+      burger.classList.remove("burger--active");
+      bodyLock.classList.remove("lock");
+    }
+  });
+  
+  menuBtn.addEventListener("click", () => {
+    burger.classList.toggle("burger--active");
+    burgerMenu.classList.toggle("burger-menu--active");
+    if (burgerMenu.classList.contains("burger-menu--active")) {
+      burger.classList.add("burger--active");
+      bodyLock.classList.add("lock");
+    } else {
+      burger.classList.remove("burger--active");
+      bodyLock.classList.remove("lock");
+    }
+  });
+
+  document.addEventListener("click", function (e) {
+    if (
+      e.target !== burger &&
+      e.target !== menuBtn &&
+      e.target !== burgerMenuWrapper
+    ) {
+      burger.classList.remove("burger--active");
+      burgerMenu.classList.remove("burger-menu--active");
+      bodyLock.classList.remove("lock");
+    }
+  });
+});
+
+// document.addEventListener("click", function (e) {
+//   // const burgerMenuWrapper = document.querySelector('.burger-menu__wrapper')
+//   if (e.target !== burger && e.target !== burgerMenu) {
+//     burger.classList.remove("burger--active");
+//     burgerMenu.classList.remove("burger-menu--active");
+//     bodyLock.classList.remove("lock");
+//   }
+// });
